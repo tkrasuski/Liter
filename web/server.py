08d445f18a4245e31d8a94ce447c0be53a2d6028
Tomek Krasuski <tkrasuski@gazeta.pl>
2022-09-12 09:59:41 +0200

    stub of app server

---
+++ web/server.py
@@ -0,0 +1,6 @@
+# This is Liter Web Client and Connectivity Application Server 
+# Server provides web gui and connectivity with other Liter servers via REST API
+
+from tornado.web import Application, RequestHandler
+from tornado.ioloop import IOLoop
+
