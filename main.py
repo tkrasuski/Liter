122fbda20a4f8b1129c558081cfcb0cae298a81b
Tomek Krasuski <tkrasuski@gazeta.pl>
2022-09-12 17:11:35 +0200

    app server devel

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import time
 import datetime
 from multiprocessing import Process, Pipe
+from web import server
 
 logging.basicConfig(filename='liter.log', encoding='utf-8', level=logging.DEBUG)
 
@@ -13,12 +14,16 @@ ds.uri = cfg.store_path
 db = ds.db
 def prepare():
     logging.debug('Starting services %s'% datetime.datetime.now())
+    server.db = db
+    p = Process(target=server.run(), args=())
+
+
 def finish():
     pass
 def run():
     RUN = True
     while RUN:
-        logging.debug('loop round')
+        logging.debug('Main loop round %s')
         time.sleep(cfg.sleep_time)
 
 if __name__ == "__main__":
@@ -26,3 +31,4 @@ if __name__ == "__main__":
     prepare()
     # execute only if run as a script
     run()
+### https://docs.python.org/3/library/multiprocessing.html
--- web/server.py
+++ web/server.py
@@ -3,4 +3,36 @@
 
 from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
+import base64 as b64
 
+db = None
+
+class Index(RequestHandler):
+    def get(self):
+        if db:
+            res = db(db.ltopic.id>0).count()
+            self.write({'message': res})
+        else:
+            self.write({'message':'no db'})
+class API_topic(RequestHandler):
+    def get(self):
+        pass
+    def post(self):
+        pass
+class API_resource(RequestHandler):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+def make_app():
+    urls=[
+        ("/", Index),
+        ("/api/topic",API_topic),
+        ("/api/resource",API_resource)]
+    return Application(urls)
+
+def run():
+    app = make_app()
+    app.listen(3000)
+    IOLoop.instance().start()
