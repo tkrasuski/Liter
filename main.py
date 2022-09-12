2c5949cb9de13c1004c692e6ece8b42b8979fd34
Tomek Krasuski <tkrasuski@gazeta.pl>
2022-09-12 10:45:51 +0200

    minor changes

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import params as cfg
 from datastore import store as ds
 import logging
 import time
+import datetime
+from multiprocessing import Process, Pipe
+
 logging.basicConfig(filename='liter.log', encoding='utf-8', level=logging.DEBUG)
 
 # setting up datastore
 ds.uri = cfg.store_path
 db = ds.db
-
+def prepare():
+    logging.debug('Starting services %s'% datetime.datetime.now())
+def finish():
+    pass
 def run():
     RUN = True
     while RUN:
@@ -16,5 +22,7 @@ def run():
         time.sleep(cfg.sleep_time)
 
 if __name__ == "__main__":
+    # starting services
+    prepare()
     # execute only if run as a script
     run()
